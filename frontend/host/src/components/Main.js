5270469c924e796c6821f1674612296580f2f511
aadanilov <danilow.aleksei@gmail.com>
2024-08-12 23:50:37 +0300

    спринт 1 задание 1

--- frontend/host/src/components/Main.js
+++ frontend/host/src/components/Main.js
@@ -18,19 +18,7 @@ function Main({ cards, onEditProfile, onAddPlace, onEditAvatar, onCardClick, onC
         </div>
         <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
       </section>
-      <section className="places page__section">
-        <ul className="places__list">
-          {cards.map((card) => (
-            <Card
-              key={card._id}
-              card={card}
-              onCardClick={onCardClick}
-              onCardLike={onCardLike}
-              onCardDelete={onCardDelete}
-            />
-          ))}
-        </ul>
-      </section>
+      <Feed/>
     </main>
   );
 }
